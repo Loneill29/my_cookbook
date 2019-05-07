33172979f96f5c1d7f7957e74800c318eefdfa78
Linda ONeill <oneill.linda21@gmail.com>
2019-05-07 10:07:46 -0500

    add CRUD views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,11 +4,54 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new
+    @recipe.title = params[:recipe][:title]
+    @recipe.body = params[:recipe][:body]
+
+    if @recipe.save
+      flash[:notice] = "Recipe was saved."
+      redirect_to @recipe
+    else
+      flash.now[:alert] = "There was an error saving the recipe. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.title = params[:recipe][:title]
+    @recipe.body = params[:recipe][:body]
+
+    if @recipe.save
+      flash[:notice] = "Recipe was updated."
+      redirect_to @recipe
+    else
+      flash.now[:alert] = "There was an error updating the recipe. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.destroy
+      flash[:notice] = "Recipe was deleted successfully."
+      redirect_to recipes_path
+    else
+      flash.now[:alert] = "There was an error deleting the recipe. Please try again."
+      render :show
+    end
+  end 
 end
